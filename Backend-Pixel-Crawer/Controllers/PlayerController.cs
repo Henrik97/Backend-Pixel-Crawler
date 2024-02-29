cc460eafb830fb7ab8486f053a82a0417854861f
Henrik97 <henrikwoodgates97@gmail.com>
2024-02-29 21:22:37 +0100

    Add project files.

---
+++ Backend-Pixel-Crawer/Controllers/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedLibrary;
+
+namespace Backend_Pixel_Crawer.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayerController : ControllerBase
+    {
+        [HttpGet]
+        public Player get(  )
+        {
+            var player = new Player();
+
+            return player;
+        }
+    }
+}
