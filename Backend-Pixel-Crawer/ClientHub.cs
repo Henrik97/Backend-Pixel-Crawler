4aeb5372550e7cba98b4322dd79aaa15bd532970
Henrik97 <henrikwoodgates97@gmail.com>
2024-03-05 12:13:50 +0100

    fixining spelling

---
+++ Backend-Pixel-Crawer/ClientHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace Backend_Pixel_Crawler
+{
+    public class ClientHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId} has joined");
+        }
+    }
+}
