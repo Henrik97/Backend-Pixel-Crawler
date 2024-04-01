1248b73aa43c5ba1caa9fa23861f3e6a1604d9f0
Valdemar Orla Nielsen <s215798@dtu.dk>
2024-04-01 12:46:31 +0200

    .

--- Backend-Pixel-Crawer/Startup.cs
+++ Backend-Pixel-Crawer/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+/*
 namespace OurNamespace
 {
     public class Startup
@@ -54,3 +54,4 @@ namespace OurNamespace
         }
     }
 }
+*/
