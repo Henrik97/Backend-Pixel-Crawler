d0b7b83296dbb76cb254b80be64c621bcaac4612
seburu <sebynoah@gmail.com>
2024-04-03 11:22:07 +0200

    TCP update

--- Backend-Pixel-Crawer/Network/Transport/TCP/TcpServer.cs
+++ Backend-Pixel-Crawer/Network/Transport/TCP/TcpServer.cs
@@ -1,119 +1,101 @@
-﻿using SharedLibrary;
-using System;
-using System.Net;
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
-using Newtonsoft;
-using System.Text.Json;
-using Newtonsoft.Json;
 
 namespace Backend_Pixel_Crawler.Network.Transport.TCP
 {
     public class TcpServer
     {
-
         private TcpListener _tcpListener;
-        private Dictionary<string, Lobby> lobbies;
+        private List<TcpClient> _connectedClients;
 
         public TcpServer()
         {
-            lobbies = new Dictionary<string, Lobby>();
+            _connectedClients = new List<TcpClient>();
             StartServer();
         }
 
-        public string GetAvailableLobbies()
+        public void StartServer()
         {
-
-           var listOfLobbies = lobbies.Values.Select(lobby => new
-           {
-               Id = lobby.LobbyId,
-               Status = lobby.isGameStarted ? "In Progress" : "Waiting for Players"
-           }).ToList();
-            return JsonConvert.SerializeObject(listOfLobbies);
-        }
-
-       public void StartServer()
-        {
-
-
-            var port = 13000;
-            var hostAdress = IPAddress.Parse("127.0.0.1");
-            _tcpListener = new TcpListener(hostAdress, port);
-
+            int port = 13000;
+            string hostAddress = "127.0.0.1";
+            _tcpListener = new TcpListener(System.Net.IPAddress.Parse(hostAddress), port);
             _tcpListener.Start();
 
+            Console.WriteLine("Server started...");
+
+            while (true)
+            {
+                TcpClient client = _tcpListener.AcceptTcpClient();
+                _connectedClients.Add(client); // Add client to list of connected clients
+                Console.WriteLine("New client connected.");
+
+                HandleClient(client);
+            }
+        }
+
+        private void HandleClient(TcpClient client)
+        {
+            Console.WriteLine("Handling client...");
+
+            NetworkStream stream = client.GetStream();
             byte[] buffer = new byte[1024];
+            int readTotal;
 
-    
-                using TcpClient client = _tcpListener.AcceptTcpClient();
-                var tcpStream = client.GetStream();
-
-                int readTotal;
-              
-
-                while ((readTotal = tcpStream.Read(buffer, 0, buffer.Length)) != 0)
-                {
-
-
+            while ((readTotal = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
                 string request = Encoding.ASCII.GetString(buffer, 0, readTotal);
                 string[] tokens = request.Split(',');
 
                 if (tokens[0] == "JOIN" && tokens.Length >= 4)
                 {
-
                     string lobbyId = tokens[1];
                     string playerId = tokens[2];
                     string playerName = tokens[3];
 
-                    Player player = new Player(playerId, playerName, client);
+                    Console.WriteLine($"Player {playerId} with name {playerName} has joined lobby {lobbyId}");
 
-                    if (lobbies.ContainsKey(lobbyId))
-                        {
-                        lobbies[lobbyId].AddPlayer(player);
-
-                        var anouncement = Encoding.UTF8.GetBytes($"Player has joined {lobbyId}");
-
-                        tcpStream.Write(anouncement, 0 , anouncement.Length); 
-
-                    }
-                    else
-                    {
-
-                        var response = Encoding.UTF8.GetBytes("There is no lobby with that ID");
-                        tcpStream.Write(response, 0, response.Length);
-                    }
-                 
+                    // Add player to lobby or handle accordingly
                 }
-                else if (tokens[0] == "CREATE")
+                else if (tokens[0] == "CREATE" && tokens.Length >= 3)
                 {
-
                     string playerId = tokens[1];
                     string playerName = tokens[2];
+                    //string lobbyId = tokens[3];
 
-                    Player player = new Player(playerId, playerName, client);
-                    int testLobbyID = 1;
-                    string lobbyId = testLobbyID.ToString();
-                    Lobby newLobby = new Lobby(lobbyId);
-                    newLobby.AddPlayer(player);
-                    lobbies.Add(lobbyId, newLobby); 
+                    Console.WriteLine($"New lobby created by player {playerId} with name {playerName}");
 
-                    var response = Encoding.UTF8.GetBytes(lobbyId);
-                    tcpStream.Write(response, 0, response.Length); // Send the lobby ID to the client
-
+                    // Send "SPAWN_PLAYER" signal to all connected clients
+                    SendSpawnPlayerSignal(playerId);
                 }
                 else if (tokens[0] == "LIST_LOBBIES")
                 {
-                    string playerId = tokens[1];
-                    string playerName = tokens[2];
-                    Player player = new Player(playerId, playerName, client);
-                    string lobbyList = GetAvailableLobbies();
-                    player.SendMessage(lobbyList);
+                    // List lobbies or handle accordingly
                 }
-
             }
 
-            }
+            stream.Close();
+            client.Close();
         }
 
-    }
+        private void SendSpawnPlayerSignal(string playerId)
+        {
+            Console.WriteLine("Sending SPAWN_PLAYER signal...");
 
+            // Construct the message to send to clients
+            string message = $"SPAWN_PLAYER,{playerId}";
+
+            // Convert the message to bytes
+            byte[] dataToSend = Encoding.UTF8.GetBytes(message);
+
+            // Send the message to all connected clients
+            foreach (var client in _connectedClients)
+            {
+                client.GetStream().Write(dataToSend, 0, dataToSend.Length);
+            }
+
+            Console.WriteLine("SPAWN_PLAYER signal sent to all clients.");
+        }
+    }
+}
